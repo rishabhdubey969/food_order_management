daf86e6e97bdd1a1c927596898399c2196cae2d0
Rishabh <rishabh.dubey1@appinventiv.com>
2025-05-09 23:10:15 +0530

    added user service

---
+++ user-service/src/auth/auth.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, UsePipes } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  signUp(@Body() createAuthDto: CreateAuthDto) {
+    return this.authService.signUpService(createAuthDto);
+  }
+
+  @Post('login')
+  login() {
+    return this.authService.loginService();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.authService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+    return this.authService.update(+id, updateAuthDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.authService.remove(+id);
+  }
+}
