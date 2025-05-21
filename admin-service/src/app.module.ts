23230135648aff7c310766b53b705a8450660753
Rishabh Dubey <rishabh.dubey1@appinventiv.com>
2025-05-21 11:00:05 +0530

    feat: add user module on admin-service

--- admin-service/src/app.module.ts
+++ admin-service/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './api/auth/auth.module';
+import { UserModule } from './api/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
