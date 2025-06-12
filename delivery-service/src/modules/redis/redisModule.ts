b0f59ef654912d4cb10e7ff1ee562a209bc23a38
Yesho Dhawal <31882cs@gmail.com>
2025-06-13 00:34:34 +0530

    change in delivery-service

--- delivery-service/src/modules/redis/redisModule.ts
+++ delivery-service/src/modules/redis/redisModule.ts
@@ -1,23 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CacheModule } from '@nestjs/cache-manager';
 import { RedisService } from './redisService';
-import { redisStore } from 'cache-manager-redis-store'
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 
 @Module({
   imports: [
     ConfigModule
-  //   CacheModule.registerAsync({
-  //     imports: [ConfigModule],
-  //     inject:[ConfigService],
-  //     useFactory: async (configService: ConfigService) => ({
-  //     store: await redisStore({
-  //     port: configService.get<string>('redis.port'),
-  //     host: configService.get<string>('redis.host')
-  //     })
-  //   })
-  // })
   ],
   providers:[RedisService],
   exports:[RedisService]
