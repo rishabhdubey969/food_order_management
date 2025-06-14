9de7c5b46bfd2122f2fb8d3f7d33a878d5c12450
Rishabh Dubey <rishabh.dubey1@appinventiv.com>
2025-06-14 10:32:46 +0530

    fix: reset password issue

--- user-service/src/api/auth/token.service.ts
+++ user-service/src/api/auth/token.service.ts
@@ -22,7 +22,8 @@ export class TokenService {
   }
 
   async validate(token: string): Promise<string | null> {
-  return await this.redisService.get(`reset:${token}`); 
+    const hashed = createHash('sha256').update(token).digest('hex'); // Hash incoming token
+  return await this.redisService.get(`reset:${hashed}`); 
   }
 
   async remove(token: string): Promise<void> {
