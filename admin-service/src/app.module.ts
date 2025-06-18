3f603196e7f2256567e3d39a0a7e212c07f89764
priyanshi-girdhar <priyanshi1@gmail.com>
2025-06-18 13:45:51 +0530

    removed config folder

--- admin-service/src/app.module.ts
+++ admin-service/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import jwtConfig from './api/user/config/jwt.config';
-import userConfig from './api/user/config/user.config'
+
+
 import { AuthModule } from './api/auth/auth.module';
 import { UserModule } from './api/user/user.module'
 import { MongooseModule } from '@nestjs/mongoose';
