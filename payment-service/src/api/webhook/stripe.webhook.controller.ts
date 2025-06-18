d2ef86677379544b944183ac726479d02bf40a7f
Shubhanshu145 <strivedi.lps@gmail.com>
2025-06-18 12:49:04 +0530

    payment-service

--- payment-service/src/api/webhook/stripe.webhook.controller.ts
+++ payment-service/src/api/webhook/stripe.webhook.controller.ts
@@ -12,7 +12,7 @@ import { Request, Response } from 'express';
 import { StripeWebhookService } from './stripe.webhook.service';
 import { StripeConfigService } from '../../config/stripe.config';
 import { ERROR, ROUTES, SUCCESS } from './constant/message.constant';
-import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
+import { ResponseMessage } from 'src/decorators/response-message.decorator';
 
 @Controller(ROUTES.WEBHOOK)
 export class StripeWebhookController {
@@ -23,7 +23,7 @@ export class StripeWebhookController {
     private readonly stripeConfig: StripeConfigService,
   ) {}
 
-  @Post(ROUTES.WEBHOOK)
+  @Post(ROUTES.STRIPE)
   @ResponseMessage(SUCCESS.WEBHOOOK_SUCCES)
   // @HttpCode(200)
   async handleWebhook(
