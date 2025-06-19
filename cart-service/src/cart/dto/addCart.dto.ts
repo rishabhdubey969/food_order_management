006b11eac15ae4c26053f7771dbbfcfbd552b025
shivani <shivani.rana@appinventiv.com>
2025-06-19 11:43:17 +0530

    cart

--- cart-service/src/cart/dto/addCart.dto.ts
+++ cart-service/src/cart/dto/addCart.dto.ts
@@ -1,4 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class AddCartDto {
-    restaurantId: string;
-    itemId: string;
-  }
+  @ApiProperty({
+    description: 'The ID of the restaurant',
+    example: '64a51abab85e4eea0294410',
+  })
+  restaurantId: string;
+
+  @ApiProperty({
+    description: 'The ID of the item to be added to the cart',
+    example: '63d3dcb6728e2e8cc8dd6e4',
+  })
+  itemId: string;
+}
